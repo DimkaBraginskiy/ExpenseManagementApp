becc9703eeacdeaee1adddf2156557bd8cfdebd9
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-08-26 11:00:32 +0200

    feat: initial setup

---
+++ ExpensesManagementApp/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesManagementApp.Data;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext() { }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
