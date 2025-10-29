3caf7f2a7dc550586ae15d8d763ae35026e7d6e0
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-10-30 00:20:27 +0100

    feat: create minimal frunctional redirection to blank dashboard

---
+++ ExpensesManagementApp/ClientApp/services/AuthService.tsx
@@ -0,0 +1,20 @@
+﻿export const authService = {
+    
+    saveToken: (token: string, refreshToken: string) => {
+        localStorage.setItem('access_token', token); 
+        localStorage.setItem('refresh_token', refreshToken);
+    },
+    
+    getToken: ()=>{
+        return localStorage.getItem('access_token');
+    },
+    
+    isLoggedIn: () => {
+        return !!localStorage.getItem('access_token')
+    },
+
+    clearTokens: () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+    }
+};
