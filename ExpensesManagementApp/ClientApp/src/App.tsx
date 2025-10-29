404939ba43f6d3de77e77a5496aba1aba08c8a97
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-10-29 20:17:15 +0100

    feat: add page management and login page

--- ExpensesManagementApp/ClientApp/src/App.tsx
+++ ExpensesManagementApp/ClientApp/src/App.tsx
@@ -1,81 +1,19 @@
-import { useState } from 'react'
 import './App.css'
-import * as React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Register} from "./pages/Register.tsx";
+import {Login} from "./pages/Login.tsx";
+import {Dashboard} from "./pages/Dashboard.tsx";
+
 
 function App() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
-    
-    
-    const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault()
-        setIsLoading(true)
-        
-        try{
-            
-            const response = await fetch('/api/Auth/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify({ email, password})
-            })
-            
-            if(response.ok){
-                alert('Registration succeeded')
-                
-                setEmail('')
-                setPassword('')
-            }else{
-                alert('Registration failed')
-            }
-            
-        }catch (error){
-            alert('Error:' + error)
-            console.error('Error: ' + error)
-        }finally {
-            setIsLoading(false)
-        }
-    }
-
-  return (
-    <div className="App">
-        
-        <form onSubmit={handleRegister}>
-
-            <div>
-                <label>Email</label>
-                <input 
-                    type="email" 
-                    id="email" 
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    placeholder="example@gmail.com"
-                />
-            </div>
-            
-            <div>
-                <label>Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(p) => setPassword(p.target.value)}
-                    required
-                    placeholder="Enter your password here"
-                />
-            </div>
-            
-            <div>
-                <button
-                    type="submit"
-                    id="submit"
-                    disabled={isLoading}
-                >
-                    {isLoading ? 'Registering...' : 'Register'}
-                </button>
-            </div>
-        </form>
-    </div>
-  )
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/register"} element={<Register/>} />
+                <Route path={"/login"} element={<Login/>} />
+                <Route path={"/dashboard"} element={<Dashboard/>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
 export default App
---
+++ ExpensesManagementApp/ClientApp/src/pages/Login.tsx
@@ -0,0 +1,82 @@
+﻿import {useState} from "react";
+import * as React from "react";
+
+export function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        try{
+
+            const response = await fetch('/api/Auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ email, password})
+            })
+
+            if(response.ok){
+                alert('Login succeeded')
+                
+                setEmail('')
+                setPassword('')
+            }else{
+                alert('Login failed')
+            }
+
+        }catch (error){
+            alert('Error:' + error)
+            console.error('Error: ' + error)
+        }finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <div className="App">
+
+            <h1>Login</h1>
+
+            <form onSubmit={handleLogin}>
+
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email} //very important to clear the field correctly
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        placeholder="example@gmail.com"
+                    />
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(p) => setPassword(p.target.value)}
+                        required
+                        placeholder="Enter your password here"
+                    />
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        id="submit"
+                        disabled={isLoading}
+                    >
+                        {isLoading ? 'Logging in...' : 'Login'}
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
